e9d92f3c10f51870152974b54b3584998735d7df
cancc <1051024728@qq.com>
2018-07-11 23:43:23 +0800

    projec t init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="home">{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            msg: 'Home'
+        }     
+    }
+}
+</script>
+
+<style>
+
+</style>
