a283fc65fab5a20d021b498ee323b1131dc2e82e
cancc <1051024728@qq.com>
2018-07-31 16:20:45 +0800

    finish one step

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="app"> 
-    <router-view/>
+  <div id="app">
+    <keep-alive>
+      <router-view/>
+    </keep-alive> 
+    
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,52 +10,63 @@
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
-import HomeIcon from './components/Icon'
-import HomeRecommend from './components/Recommend'
-import HomeWeekend from './components/Weekend'
-import axios from 'axios'
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcon from "./components/Icon";
+import HomeRecommend from "./components/Recommend";
+import HomeWeekend from "./components/Weekend";
+import axios from "axios";
+import { mapState } from "vuex";
 export default {
-    name: 'Home',
-    data() {
-        return {
-            swiperList: [],
-            iconList: [],
-            recommendList: [],
-            weekendList: []
-        }     
-    },
-    methods: {
-        getHomeInfo() {
-            axios.get('/api/index.json')
-                .then((res) => {
-                    if(res.data.code === 0) {
-                        let info = res.data
-                        console.log(info)       
-                        this.iconList = info.data.iconList
-                        this.swiperList = info.data.swiperList
-                        this.recommendList = info.data.recommendList
-                        this.weekendList = info.data.weekendList
-                    }else{
-                            console.log("error")
-                    }
-                })
+  name: "Home",
+  data() {
+    return {
+      lastCity: "",
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    };
+  },
+  computed: {
+    ...mapState(["city"])
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get("/api/index.json").then(res => {
+        if (res.data.code === 0) {
+          let info = res.data;
+          console.log(info);
+          this.iconList = info.data.iconList;
+          this.swiperList = info.data.swiperList;
+          this.recommendList = info.data.recommendList;
+          this.weekendList = info.data.weekendList;
+        } else {
+          console.log("error");
         }
-    },
-    mounted () {
-     this.getHomeInfo()
-    },
-    components: {
-        HomeHeader,
-        HomeSwiper,
-        HomeIcon,
-        HomeRecommend,
-        HomeWeekend
+      });
     }
-}
+  },
+  mounted() {
+		this.lastCity = this.city
+    this.getHomeInfo();
+	},
+	activated () {
+		
+		if(this.lastCity!==this.city) {
+			this.lastCity = this.city
+			this.getHomeInfo()
+		}
+	},
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcon,
+    HomeRecommend,
+    HomeWeekend
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
